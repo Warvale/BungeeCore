98fe0d8ec8bd59f5a9349a69ad6247db4a909018
RONTheCookie <ronthecookie0101@gmail.com>
2017-10-07 00:56:27 +0300

    clean imports

--- src/main/java/net/warvale/bungeecore/MSGCommand.java
+++ src/main/java/net/warvale/bungeecore/MSGCommand.java
@@ -6,7 +6,6 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
-import sun.plugin2.message.Message;
 
 public class MSGCommand extends Command {
 
